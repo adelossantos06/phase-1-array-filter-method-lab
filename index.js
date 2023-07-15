c0055b6d0165f81d16a9cc168c9fb6a1347bff6f
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-07-14 20:05:42 -0500

    completes filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+
+
+
+function findMatching(array, query) {
+    const lower = query.toLowerCase()
+    const result = array.filter(el => el.toLowerCase() === lower)
+    return result
+}
+
+
+function fuzzyMatch(array, query) {
+    let result = array.filter(el => el.toLowerCase().startsWith(query.toLowerCase()))
+    return result
+}
+
+function matchName(array, query) {
+    let result = array.filter(el => el.name === query)
+    return result
+}
